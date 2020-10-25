f195b575321089829be464645fefbb1086272430
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2020-10-25 19:32:55 -0300

    Patrones

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections;
+using System.Text;
 
 namespace Full_GRASP_And_SOLID.Library
 {
@@ -13,17 +14,6 @@ namespace Full_GRASP_And_SOLID.Library
     {
         private ArrayList steps = new ArrayList();
 
-        public ArrayList Steps
-        {
-            get
-            {
-                return steps;
-            }
-            private set
-            {
-                Steps=value;
-            }
-        }
         public Product FinalProduct { get; set; }
 
         public void AddStep(Step step)
@@ -34,6 +24,22 @@ namespace Full_GRASP_And_SOLID.Library
         public void RemoveStep(Step step)
         {
             this.steps.Remove(step);
+        }
+        /// <summary>
+        /// Se cumple con Expert ya que la receta que conoce los datos a imprimir genera el texto a
+        /// imprimir. Tambien se logra mantener la encapsulacion.
+        /// </summary>
+        /// <returns>texto a imprimir</returns>
+        public string StringToPrint()
+        {
+            string result="";
+            result+= $"Receta de {this.FinalProduct.Description}: \n";
+            foreach (Step step in this.steps)
+            {
+                result+= step.StepText();
+                result+="\n";
+            }
+            return result;
         }
     }
 }
